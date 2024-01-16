eb7b55108f9a83b9af7c738f41cd4f93d66b4377
Alexandre <alexandre.daby-seesaram@polytechnique.edu>
2024-01-16 16:52:49 +0100

    Clean comments relative to the displacement of the weights update in the forward functions

--- HiDeNN_PDE.py
+++ HiDeNN_PDE.py
@@ -22,10 +22,8 @@ class LinearLeft(nn.Module):
         super(LinearLeft, self).__init__()
         self.relu = nn.ReLU()
         self.l1 = nn.Linear(1,1)
-        # self.l1.weight.data.fill_(-1/(self.x_i-self.x_im1))
         self.l1.bias.data.fill_(1)
         self.l1.bias.requires_grad = False
-        # self.l1.weight.requires_grad = False
 
     def forward(self,x, x_im1, x_i):
         self.l1.weight.data.fill_(-1/(x_i-x_im1))
@@ -44,10 +42,8 @@ class LinearRight(nn.Module):
         super(LinearRight, self).__init__()
         self.relu = nn.ReLU()
         self.l1 = nn.Linear(1,1)
-        # self.l1.weight.data.fill_(-1/(self.x_ip1-self.x_i))
         self.l1.bias.data.fill_(1)
         self.l1.bias.requires_grad = False
-        # self.l1.weight.requires_grad = False
 
     def forward(self,x,x_ip1,x_i):
         self.l1.weight.data.fill_(-1/(x_ip1-x_i))
@@ -61,22 +57,14 @@ class Shapefunction(nn.Module):
     See [Zhang et al. 2021] consisting of a linear(1,2) function leading the the Linear{Left+Right} layers which output then passes through Linear(2,1). 
     The input parameters are:
         - the coordinate x where the function is evaluated
-        - the left node of the element x_im1
-        _ the right node of the element x_ip1
-        - the node associated to the shape functino being built x_i   
+        - the index i of the node associated to the shape function 
         
-        All the weights and biais are set as untranable and are evaluated given the nodal position fed while initialising the network
-         /!\ 
-        / ! \ see if it they get updated once the weight of the "coordinates" in the MeshNN layer have been updated"""
+        All the weights and biais are set as untranable and are evaluated given the nodal position fed while initialising the network"""
     
     def __init__(self, i):
         super(Shapefunction, self).__init__()
-        # Node associated to the shape function
+        # Index of the node associated to the shape function
         self.i = i
-        # Local neighbours coordinates
-        # self.x_im1 = x_im1
-        # self.x_i = x_i
-        # self.x_ip1 = x_ip1
         # Layer 1
         self.l1 = nn.Linear(1,2)
         # Set weight and bias for first layer and freeze them
@@ -94,11 +82,14 @@ class Shapefunction(nn.Module):
         self.Linears = nn.ModuleList([LinearLeft(),LinearRight()])
     
     def forward(self, x, coordinates):
+        """ The forward function takes as an input the coordonate x at which the NN is evaluated and the parameters' list coordinates where the nodes' corrdinates of the mesh are stored"""
         i = self.i
+        # For the SF on the left
         if i == -1:
             x_i = coordinates[i]
             x_im1 = coordinates[i]-1/1000
             x_ip1 = coordinates[i+1]
+        # For the SF on the right
         elif i == -2:
             x_i = coordinates[i]
             x_im1 = coordinates[i-1]
@@ -111,11 +102,8 @@ class Shapefunction(nn.Module):
         self.l1.bias[1].data.fill_(-x_i) 
         self.l1.bias.requires_grad = False
         l1 = self.l1(x)
-        # l2 = torch.empty((2,1),dtype=torch.float32)
         top = self.Linears[0](l1[:,0].view(-1,1),x_im1,x_i)
         bottom = self.Linears[1](l1[:,1].view(-1,1),x_ip1,x_i)
-        # top = self.Linears[0](l1[0:1])
-        # bottom = self.Linears[1](l1[1:2])
         l2 = torch.empty((bottom.shape[0],2),dtype=torch.float32)
         l2[:,0] = top[:,0]
         l2[:,1] = bottom[:,0]
@@ -128,25 +116,18 @@ class MeshNN(nn.Module):
     The Interpolation layer weights correspond to the nodal values. Updating them is equivqlent to solving the PDE. """
     def __init__(self, np, L):
         super(MeshNN, self).__init__()
-        # self.coordinates = nn.Parameter(data=torch.linspace(0,L,np), requires_grad=True)
         self.coordinates = nn.ParameterList([nn.Parameter(torch.tensor(i)) for i in torch.linspace(0,L,np)])
         self.np = np 
         self.L = L 
         self.Functions = nn.ModuleList([Shapefunction(i) for i in range(1,np-1)])
-        # self.Functions.insert(0, Shapefunction(self.coordinates[0]-self.L/1000, self.coordinates[0],self.coordinates[0+1]))
-        # self.Functions.append(Shapefunction(self.coordinates[self.np-2], self.coordinates[self.np-1],self.coordinates[self.np-1]+self.L/1000))
         self.InterpoLayer_uu = nn.Linear(self.np-2,1,bias=False)
-        # self.NodalValues = nn.Parameter(data=torch.linspace(0,L,np), requires_grad=False)
-        # self.NodalValues_uu = nn.Parameter(data=torch.linspace(0,L,np-2), requires_grad=False)
         self.NodalValues_uu = nn.Parameter(data=torch.ones(np-2), requires_grad=False)
         self.InterpoLayer_uu.weight.data = self.NodalValues_uu
-        # self.InterpoLayer.weight.requires_grad = False # To fix the nodal value and update solely the coordinates
         self.Functions_dd = nn.ModuleList([Shapefunction(-1),Shapefunction(-2)])
         self.InterpoLayer_dd = nn.Linear(2,1,bias=False)
         self.InterpoLayer_dd.weight.requires_grad = False
         self.SumLayer = nn.Linear(2,1,bias=False)
         self.SumLayer.weight.data.fill_(1)
-        # self.SumLayer.weight[0,1].data.fill_(0) # Debug to remove the influence of ddl_d
         self.SumLayer.weight.requires_grad = False
 
 
@@ -157,7 +138,6 @@ class MeshNN(nn.Module):
         out_dd = torch.stack(intermediate_dd)
         u_uu = self.InterpoLayer_uu(out_uu.T)
         u_dd = self.InterpoLayer_dd(out_dd.T)
-        # u = torch.empty((u_uu.shape[0],2),dtype=torch.float32)
         u = torch.empty((2,u_uu.shape[1]),dtype=torch.float32)
         u[0,:] = u_uu[0,:]
         u[1,:] = u_dd[0,:]       
@@ -219,7 +199,7 @@ def AnalyticSolution(A,E,x):
 
 #%% Training loop
 
-TrialCoordinates = torch.tensor([[i/10] for i in range(2,100)], dtype=torch.float32, requires_grad=True)
+TrialCoordinates = torch.tensor([[i/10] for i in range(2,10)], dtype=torch.float32, requires_grad=True)
 
 error = []
 for epoch in range(n_epochs):

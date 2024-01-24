2b0b7b36cdaa82e07c29dbf15418a8f4c29087aa
Alexandre <alexandre.daby-seesaram@polytechnique.edu>
2024-01-24 11:26:03 +0100

    Moves the definition of mechanical functions to different file

--- HiDeNN_PDE.py
+++ HiDeNN_PDE.py
@@ -2,9 +2,6 @@
 import torch
 import random
 import torch.nn as nn
-import numpy as np
-
-
 
 #%% Define the model for a 1D linear Beam mesh
 class LinearLeft(nn.Module):
@@ -183,71 +180,26 @@ MeshBeam = MeshNN(np,L)     # Creates the associated model
 u_0 = 0                     #Left BC
 u_L = 0                     #Right BC
 MeshBeam.SetBCs(u_0,u_L)
+# Import mechanical functions
+
+from Bin.PDE_Library import RHS, PotentialEnergy, \
+    PotentialEnergyVectorised, AlternativePotentialEnergy, \
+        Derivative, AnalyticGradientSolution, AnalyticSolution
+
+
 # Set the coordinates as trainable
 MeshBeam.UnFreeze_Mesh()
+# Set the require output requirements
 BoolPlot = False             # Bool for plots used for gif
 BoolCompareNorms = True      # Bool for comparing energy norm to L2 norm
-MSE = nn.MSELoss()
 
 
 #%% Define loss and optimizer
 learning_rate = 0.001
 n_epochs = 5000
 optimizer = torch.optim.Adam(MeshBeam.parameters(), lr=learning_rate)
-import numpy as np
+MSE = nn.MSELoss()
 
-def RHS(x):
-    """Defines the right hand side (RHS) of the equation (the body force)"""
-    b = -(4*np.pi**2*(x-2.5)**2-2*np.pi)/(torch.exp(np.pi*(x-2.5)**2)) \
-        - (8*np.pi**2*(x-7.5)**2-4*np.pi)/(torch.exp(np.pi*(x-7.5)**2))
-    return  b
-
-def PotentialEnergy(A,E,u,x,b):
-    """Computes the potential energy of the Beam, which will be used as the loss of the HiDeNN"""
-    du_dx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    integral = 0
-    for i in range(1,u.data.shape[0]):
-        integral += (0.25*A*E*(x[i]-x[i-1])*(du_dx[i]**2+du_dx[i-1]**2)) \
-            - 0.5*((x[i]-x[i-1])*(u[i]*b[i]+u[i-1]*b[i-1]))
-    return integral
-
-def PotentialEnergyVectorised(A, E, u, x, b):
-    """Computes the potential energy of the Beam, which will be used as the loss of the HiDeNN"""
-    du_dx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    # Calculate dx
-    dx = x[1:] - x[:-1]
-    # Vectorised calculation of the integral terms
-    int_term1 = 0.25 * A * E * dx * (du_dx[1:]**2 + du_dx[:-1]**2)
-    int_term2 = 0.5 * dx * (u[1:] * b[1:] + u[:-1] * b[:-1])
-
-    # Vectorised calculation of the integral using the trapezoidal rule
-    integral = torch.sum(int_term1 - int_term2)
-
-    return integral
-
-def AlternativePotentialEnergy(A,E,u,x,b):
-    du_dx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    f_x = 0.5*(A*E*du_dx**2) - u*b
-    f_x = f_x.view(-1)
-    dx = torch.diff(x.view(-1))
-    av = 0.5*(f_x[1:]+f_x[:-1])*dx
-    return torch.sum(av)
-
-def Derivative(u,x):
-    du_dx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
-    return du_dx
-
-def AnalyticSolution(A,E,x):
-    out = (1/(A*E)*(torch.exp(-np.pi*(x-2.5)**2)-np.exp(-6.25*np.pi))) \
-        + (2/(A*E)*(torch.exp(-np.pi*(x-7.5)**2)-np.exp(-56.25*np.pi))) \
-            - (x/(10*A*E))*(np.exp(-6.25*np.pi) - np.exp(-56.25*np.pi))
-    return out
-
-def AnalyticGradientSolution(A,E,x):
-    out = (2/(A*E)*((-np.pi)*(x-2.5)*torch.exp(-np.pi*(x-2.5)**2))) \
-        + (4/(A*E)*((-np.pi)*(x-7.5)*torch.exp(-np.pi*(x-7.5)**2))) \
-            - (1/(10*A*E))*(np.exp(-6.25*np.pi) - np.exp(-56.25*np.pi))
-    return out
 
 #%% Training loop
 TrialCoordinates = torch.tensor([[i/50] for i in range(2,500)], 

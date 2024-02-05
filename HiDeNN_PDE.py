67aa7baa938f877404daee50159febce73fcb0fc
Skardova Katerine <skardova@Mac-Skardova.saclay.inria.fr>
2024-02-05 10:25:17 +0100

    Element-based approach, initial test

--- HiDeNN_PDE.py
+++ HiDeNN_PDE.py
@@ -71,6 +71,92 @@ class LinearBlock(nn.Module):
         mid = (y_b-y_a)*mid + y_a
         return mid
 
+class ShapefunctionQuadratic(nn.Module):
+    """This is an implementation of quadratic 1D shape function in a DNN representation
+    See [Zhang et al. 2021] consisting of a linear(1,2) function leading the the Linear{Left+Right} layers which output then passes through Linear(2,1). 
+    The input parameters are:
+        - the coordinate x where the function is evaluated
+        - the index i of the node associated to the shape function """
+    
+    def __init__(self, i):
+        super(ShapefunctionQuadratic, self).__init__()
+        # Index of the node associated to the shape function
+        self.i = i
+        # Defines the linear block function
+        self.LinearBlock = LinearBlock()
+        self.Multiplication = MultiplicationBlock()
+        self.relu = nn.ReLU()
+
+        # Defines threshold so that two coordinates cannot go too close to one another
+        self.threshold_p = torch.tensor(1-1/150,dtype=torch.float64)
+        self.threshold_m = torch.tensor(1+1/150,dtype=torch.float64)
+    
+    def forward(self, x, coordinates):
+        """ The forward function takes as an input the coordonate x at which the NN is evaluated and the parameters' list coordinates where the nodes' corrdinates of the mesh are stored"""
+        i = self.i
+        # For the SF on the left
+        if i == -1: # for the outter Shape functions the index acts as a tag, -1 for the left, -2 for the right
+            x_i = coordinates[0]
+            x_im1 = coordinates[0]-coordinates[-1]*1/100
+            x_ip1 = coordinates[1]
+        # For the SF on the right
+        elif i == -2: # for the outter Shape functions the index acts as a tag, -1 for the left, -2 for the right
+            x_i = coordinates[-1]
+            x_im1 = coordinates[-2]
+            x_ip1 = coordinates[-1]*(1+1/100)
+        else:
+            x_i = coordinates[i]
+            x_im1 = coordinates[i-1]
+            x_ip1 = coordinates[i+1] 
+
+        #  Stop nodes from getting too close 
+        x_i = torch.minimum(x_i, self.threshold_p*x_ip1)
+        x_i = torch.maximum(x_i, self.threshold_m*x_im1)
+
+
+        left = self.LinearBlock(x, self.relu(x_i), self.relu(x_ip1), 0, x_ip1 - x_i)
+        right = self.LinearBlock(x, x_i, self.relu(x_ip1), self.relu(x_ip1) - x_i, 0)
+        
+        prod = -self.Multiplication(left,right)/(((x_i + x_ip1)*0.5 - x_i)*((x_i + x_ip1)*0.5 - x_ip1))
+        #prod = -(left*right)/(((x_i + x_ip1)*0.5 - x_i)*((x_i + x_ip1)*0.5 - x_ip1))
+        return prod
+
+
+class ElementBlock(nn.Module):
+
+    def __init__(self, i ):
+        super(ElementBlock, self).__init__()
+        self.i = i
+        self.LinearBlock = LinearBlock()
+
+    def forward(self, x, coordinates):
+        i = self.i
+        print("  Element no = ", i)
+
+        # for the outter Shape functions the index acts as a tag, -1 for the left, -2 for the right
+        if i == -1:     # Left
+            x_left = coordinates[0]
+            x_right = coordinates[1]
+            x_im1 = coordinates[0]-coordinates[-1]*1/100
+            x_ip1 = coordinates[2]
+
+        elif i == -2:   # Right
+            x_right = coordinates[-1]
+            x_left = coordinates[-2]
+            x_im1 = coordinates[-3]
+            x_ip1 = coordinates[-1]*(1+1/100)
+        else:
+            x_left = coordinates[i]
+            x_right = coordinates[i+1]
+            x_im1 = coordinates[i-1] 
+            x_ip1 = coordinates[i+2] 
+ 
+        left = self.LinearBlock(x, x_left, x_right, 0, 1)
+        right = self.LinearBlock(x, x_left, x_right, 1, 0)
+
+        out = torch.cat((left, right),dim=1)
+
+        return out
 
 class Shapefunction(nn.Module):
     """This is an implementation of linear 1D shape function in a DNN representation
@@ -83,13 +169,6 @@ class Shapefunction(nn.Module):
         super(Shapefunction, self).__init__()
         # Index of the node associated to the shape function
         self.i = i
-        # Layer end
-        self.l2 = nn.Linear(2,1)
-        # Set weight and bias for end layer and freeze them
-        self.l2.weight.data.fill_(1)
-        self.l2.bias.data.fill_(-1)
-        self.l2.bias.requires_grad = False
-        self.l2.weight.requires_grad = False
         # Defines the linear block function
         self.LinearBlock = LinearBlock()
         # Defines threshold so that two coordinates cannot go too close to one another
@@ -128,37 +207,59 @@ class MeshNN(nn.Module):
     """This is the main Neural Network building the FE interpolation, the coordinates parameters are trainable are correspond to the coordinates of the nodes in the Mesh which are passed as parameters to the sub NN where they are fixed. 
     Updating those parameters correspond to r-adaptativity
     The Interpolation layer weights correspond to the nodal values. Updating them is equivqlent to solving the PDE. """
-    def __init__(self, np, L, alpha = 0.005):
+    def __init__(self, n_elem, L, connectivity_matrix, alpha = 0.005):
         super(MeshNN, self).__init__()
         self.alpha = alpha # set the weight for the Mesh regularisation 
-        self.coordinates = nn.ParameterList([nn.Parameter(torch.tensor([[i]])) for i in torch.linspace(0,L,np)])
-        self.np = np 
+
+        self.elements = torch.arange(n_elem)
+        self.n_elem = n_elem
+        self.np = n_elem+1 
+        self.connectivity_matrix = connectivity_matrix
+        self.coordinates = nn.ParameterList([nn.Parameter(torch.tensor([[i]])) for i in torch.linspace(0,L,self.np)])
+        
+        print("Elements = ", self.elements)
+
         self.L = L 
-        self.Functions = nn.ModuleList([Shapefunction(i) for i in range(1,np-1)])
+        self.Functions = nn.ModuleList([ElementBlock(i) for i in range(1,self.n_elem-1)])
+        
+        self.CompositionLayer = nn.Linear(2*self.n_elem,self.np,bias=False)
+        self.CompositionLayer.weight.data = self.connectivity_matrix
+
         self.InterpoLayer_uu = nn.Linear(self.np-2,1,bias=False)
-        self.NodalValues_uu = nn.Parameter(data=0.1*torch.ones(np-2), requires_grad=False)
+        self.NodalValues_uu = nn.Parameter(data=0.1*torch.ones(self.np-2), requires_grad=False)
         self.InterpoLayer_uu.weight.data = self.NodalValues_uu
-        self.Functions_dd = nn.ModuleList([Shapefunction(-1),
-                                                         Shapefunction(-2)])
-        self.InterpoLayer_dd = nn.Linear(2,1,bias=False)
+
+        self.Functions_dd = nn.ModuleList([ElementBlock(-1),ElementBlock(-2)])
+
+        self.InterpoLayer_dd = nn.Linear(1,1,bias=False)
         self.InterpoLayer_dd.weight.requires_grad = False
+
         self.SumLayer = nn.Linear(2,1,bias=False)
         self.SumLayer.weight.data.fill_(1)
         self.SumLayer.weight.requires_grad = False
 
-        self.Multiplication = MultiplicationBlock()
-
 
     def forward(self,x):
         # Compute shape functions 
-        intermediate_uu = [self.Functions[l](x,self.coordinates) for l in range(self.np-2)]
+        intermediate_uu = [self.Functions[l](x,self.coordinates) for l in range(self.n_elem-2)]
         intermediate_dd = [self.Functions_dd[l](x,self.coordinates) for l in range(2)]
+
         out_uu = torch.cat(intermediate_uu, dim=1)
+        print("out_uu = ", out_uu.shape)
         out_dd = torch.cat(intermediate_dd, dim=1)
-        u_uu = self.InterpoLayer_uu(out_uu)
-        u_dd = self.InterpoLayer_dd(out_dd)
-        u = torch.stack((u_uu,u_dd), dim=1)
-        return self.SumLayer(u), intermediate_uu, out_dd
+        #print("out_dd = ", out_dd.shape)
+        long_vector = torch.cat((out_dd,out_uu),dim=1)
+
+        recomposed_all = self.CompositionLayer(long_vector)
+        recomposed = recomposed_all[:,1:-1] - 1
+        recomposed_BC = torch.stack((recomposed_all[:,0],recomposed_all[:,-1]),dim=1)
+
+        u_inter = self.InterpoLayer_uu(recomposed)
+        u_BC = self.InterpoLayer_dd(recomposed_BC)
+
+        u = torch.stack((u_inter,u_BC), dim=1)
+        return self.SumLayer(u), recomposed, recomposed_BC
+
         ######### LEGACY  #############
         # Previous implementation (that gives slightly different results)
         # out_uu = torch.stack(intermediate_uu)
@@ -204,11 +305,39 @@ class MeshNN(nn.Module):
 #%% Application of the NN
 # Geometry of the Mesh
 L = 10                           # Length of the Beam
-np = 10                          # Number of Nodes in the Mesh
+n_elem = 10                          # Number of Nodes in the Mesh
 A = 1                            # Section of the beam
 E = 175                          # Young's Modulus (should be 175)
 alpha =0.0                   # Weight for the Mesh regularisation 
-MeshBeam = MeshNN(np,L,alpha)    # Creates the associated model
+
+connectivity_matrix = torch.zeros((n_elem+1,2*n_elem))
+
+connectivity_matrix[0,1] = 1.0          # left
+connectivity_matrix[n_elem,2] = 1.0     # right
+
+# node 1
+connectivity_matrix[1,0] = 1.0
+connectivity_matrix[1,5] = 1.0
+
+# node n-1
+connectivity_matrix[n_elem-1,3] = 1.0
+connectivity_matrix[n_elem-1,-2] = 1.0
+
+
+for node in range(2, n_elem-1):
+    row = node
+    left = node-1
+    cols = [2*(left+1), 2*(left+1)+3]
+    print("node = ", node)
+    print("cols = ", cols)
+    for col in cols:
+        connectivity_matrix[row,col] = 1
+
+
+print("connectivity matrix = ", connectivity_matrix.data)
+print()
+
+MeshBeam = MeshNN(n_elem,L,connectivity_matrix,alpha)    # Creates the associated model
 # Boundary conditions
 u_0 = 0                     #Left BC
 u_L = 0                     #Right BC
@@ -237,23 +366,17 @@ optimizer = torch.optim.SGD(MeshBeam.parameters(), lr=learning_rate)
 
 MSE = nn.MSELoss()
 
-'''                  
-a = torch.tensor([[1],[-2],[0],[0.5]], dtype=torch.float64, requires_grad=True)
-b = torch.tensor([[3],[-4],[5],[4]], dtype=torch.float64, requires_grad=True)
-prod = MeshBeam.Multiplication(a,b)
 
-print("a = ", a)
-print("b = ", b)
-print("a.b = ", prod)
-'''
-
-
-TrialCoordinates = torch.tensor([[i/50] for i in range(2,500)], 
+TrialCoordinates = torch.tensor([[i/50] for i in range(-50,550)], 
                                 dtype=torch.float64, requires_grad=True)
+InitialCoordinates = [MeshBeam.coordinates[i].data.item() for i in range(len(MeshBeam.coordinates))]
+print("InitialCoordinates = ", InitialCoordinates)
+print()
+
 pred, ShapeFunctions, ShapeFunctions_BC = MeshBeam(TrialCoordinates)
 
-InitialCoordinates = [MeshBeam.coordinates[i].data.item() for i in range(len(MeshBeam.coordinates))]
 Pplot.Slot_ShapeFuctions(TrialCoordinates.detach(), ShapeFunctions, ShapeFunctions_BC, pred, InitialCoordinates)
+
 
 
 
@@ -344,7 +467,7 @@ while epoch<n_epochs and stagnancy_counter < 50 and loss_counter<2000:
             # Copute and store the L2 error w.r.t. the analytical solution
             error2.append(MSE(AnalyticSolution(A,E,TrialCoordinates.data),u_predicted).data)
 
-    if loss_decrease >= 0 and loss_decrease < 1.0e-5:
+    if loss_decrease >= 0 and loss_decrease < 1.0e-6:
         stagnancy_counter = stagnancy_counter +1
     else:
         stagnancy_counter = 0

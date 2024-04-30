b2b6ed3d7f0c66f4adddf5bbcff499033d5252f0
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2024-04-30 16:30:29 +0200

    Before cleaning outer loop

--- main_integral_2D.py
+++ main_integral_2D.py
@@ -175,7 +175,7 @@ while n_refinement < max_refinment and not stagnation:
         Model_2D.UnFreeze_Mesh()
         Model_2D.train()
         Model_2D.RefinementParameters(  MaxGeneration = 3, 
-                                Jacobian_threshold = 0.5)
+                                Jacobian_threshold = 0.3)
     else:
         Model_2D.train()
 
@@ -203,7 +203,7 @@ sol = meshio.Mesh(Coord_converged, {"triangle":(Connect_converged-1)},
 point_data={"U":u.data}, 
 cell_data={"eps": [eps.data], "sigma": [sigma.data]}, )
 sol.write(
-    "Results/Paraview/sol_u_end_training_test_"+Name+".vtk", 
+    "Results/Paraview/sol_u_end_training_over_"+Name+".vtk", 
 )
 
 #%% Export intermediate convergence steps
@@ -217,7 +217,7 @@ for timestep in range(len(U_interm_tot)):
     cell_data={"Gen": [Gen_interm_tot[timestep]]}, )
 
     sol.write(
-        f"Results/Paraview/TimeSeries/sol_u_multiscale_autom_spliting3_"+Name+f"_{timestep}.vtk",  
+        f"Results/Paraview/TimeSeries/sol_u_multiscale_autom_over_"+Name+f"_{timestep}.vtk",  
     )
 
 # %%

27838d45789edc99dc08f71f45fb17e7d10f0ec4
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2024-08-20 16:56:49 +0200

    Clean main file

--- main.py
+++ main.py
@@ -98,20 +98,20 @@ match config["hardware"]["device"]:
 #%% Initialise material
 
 if config["interpolation"]["dimension"] == 1:
-    Mat = pre.Material(             flag_lame = True,                           # If True should input lmbda and mu instead of E and nu
-                                    coef1     = config["material"]["E"],        # Young Modulus
-                                    coef2     = config["geometry"]["A"]         # Section area of the 1D bar
+    Mat = pre.Material(             flag_lame = True,                               # If True should input lmbda and mu instead of E and nu
+                                    coef1     = config["material"]["E"],            # Young Modulus
+                                    coef2     = config["geometry"]["A"]             # Section area of the 1D bar
                         )
 elif config["interpolation"]["dimension"] == 2:
     try:
-        Mat = pre.Material(         flag_lame = False,                          # If True should input lmbda and mu instead of E and nu
-                                    coef1     = config["material"]["E"],        # Young Modulus
-                                    coef2     = config["material"]["nu"]        # Poisson's ratio
+        Mat = pre.Material(         flag_lame = False,                              # If True should input lmbda and mu instead of E and nu
+                                    coef1     = config["material"]["E"],            # Young Modulus
+                                    coef2     = config["material"]["nu"]            # Poisson's ratio
                         )
     except:
-        Mat = pre.Material(         flag_lame = True,                           # If True should input lmbda and mu instead of E and nu
-                                    coef1     = config["material"]["lmbda"],    # First Lame's coef
-                                    coef2     = config["material"]["mu"]        # Second Lame's coef
+        Mat = pre.Material(         flag_lame = True,                               # If True should input lmbda and mu instead of E and nu
+                                    coef1     = config["material"]["lmbda"],        # First Lame's coef
+                                    coef2     = config["material"]["mu"]            # Second Lame's coef
                         )
 
     
@@ -127,21 +127,21 @@ MaxElemSize = pre.ElementSize(
                             )
 Excluded = []
 Mesh_object = pre.Mesh( 
-                                config["geometry"]["Name"],                 # Create the mesh object
+                                config["geometry"]["Name"],                         # Create the mesh object
                                 MaxElemSize, 
                                 config["interpolation"]["order"], 
                                 config["interpolation"]["dimension"]
                         )
 
 Mesh_object.AddBorders(config["Borders"]["Borders"])
-Mesh_object.AddBCs(                                                         # Include Boundary physical domains infos (BCs+volume)
+Mesh_object.AddBCs(                                                                 # Include Boundary physical domains infos (BCs+volume)
                                 config["geometry"]["Volume_element"],
                                 Excluded,
                                 config["DirichletDictionryList"]
                     )                   
 
-Mesh_object.MeshGeo()                                                       # Mesh the .geo file if .msh does not exist
-Mesh_object.ReadMesh()                                                      # Parse the .msh file
+Mesh_object.MeshGeo()                                                               # Mesh the .geo file if .msh does not exist
+Mesh_object.ReadMesh()                                                              # Parse the .msh file
 
 match config["interpolation"]["dimension"]:
     case 1:
@@ -154,7 +154,7 @@ match config["interpolation"]["dimension"]:
 #     Mesh_object.ExportMeshVtk()
 
 if config["interpolation"]["dimension"] ==1 and config["solver"]["IntegralMethod"] == "Trapezoidal":
-    Mesh_object.AssemblyMatrix()                                            # Build the assembly weight matrix
+    Mesh_object.AssemblyMatrix()                                                        # Build the assembly weight matrix
 
 if int(Mesh_object.dim) != int(Mesh_object.dimension):
     raise ValueError("The dimension of the provided geometry does not match the job dimension")
@@ -164,7 +164,7 @@ if not config["solver"]["ParametricStudy"]:
     match config["interpolation"]["dimension"]:
         case 1:
             if config["solver"]["TrainingStrategy"]=="Integral":
-                match config["solver"]["IntegralMethod"]:                           # Build the model
+                match config["solver"]["IntegralMethod"]:                               # Build the model
                     case "Gaussian_quad":
                         Model_FEM = MeshNN_1D(Mesh_object, config["interpolation"]["n_integr_points"])  
                     case "Trapezoidal":
@@ -205,34 +205,29 @@ if config["solver"]["ParametricStudy"]:
                                                     config["parameters"]["para_1_max"],
                                                     config["parameters"]["N_para_1"]]])
 
-    ROM_model = NeuROM(                                                         # Build the surrogate (reduced-order) model
-                                                Mesh_object, 
-                                                ParameterHypercube, 
-                                                config,
-                                                config["solver"]["n_modes_ini"],
-                                                config["solver"]["n_modes_max"]
+    ROM_model = NeuROM(                                                                 # Build the surrogate (reduced-order) model
+                                                    Mesh_object, 
+                                                    ParameterHypercube, 
+                                                    config,
+                                                    config["solver"]["n_modes_ini"],
+                                                    config["solver"]["n_modes_max"]
                     )
 
 #%% Load coarser model  
 
-match config["solver"]["N_ExtraCoordinates"]:       # TODO: Should be a check of compatibility and name should be read from config file
+match config["solver"]["N_ExtraCoordinates"]:       
     case 2:
         match config["interpolation"]["dimension"]:
             case 1:
-                # PreviousFullModel = 'TrainedModels/1D_Bi_Stiffness_np_10'
                 PreviousFullModel = 'TrainedModels/1D_Bi_Stiffness_np_10_new'
 
             case 2:
-                # PreviousFullModel = 'TrainedModels/2D_Bi_Parameters'
-                # PreviousFullModel = 'TrainedModels/2D_Bi_Parameters_el_0.5'
                 PreviousFullModel = 'TrainedModels/2D_Bi_Parameters_el_0.2'
     case 1:
         match config["solver"]["IntegralMethod"]:
             case "Trapezoidal":
                 PreviousFullModel = 'TrainedModels/1D_Mono_Stiffness_np_100'
-                # PreviousFullModel = 'TrainedModels/1D_Mono_Stiffness_np_100_legacy'
             case "Gaussian_quad":
-                # PreviousFullModel = 'TrainedModels/1D_Mono_Stiffness_Gauss_np_40'
                 PreviousFullModel = 'TrainedModels/1D_Mono_Stiffness_Gauss_np_100'
 
 
@@ -241,14 +236,14 @@ if config["training"]["LoadPreviousModel"] and config["solver"]["ParametricStudy
     ROM_model.UnfreezeTruncated()
 #%% Training 
 if config["solver"]["ParametricStudy"]:
-    ROM_model.Freeze_Mesh()                                                         # Set space mesh cordinates as untrainable
-    ROM_model.Freeze_MeshPara()                                                     # Set parameters mesh cordinates as untrainable
+    ROM_model.Freeze_Mesh()                                                             # Set space mesh cordinates as untrainable
+    ROM_model.Freeze_MeshPara()                                                         # Set parameters mesh cordinates as untrainable
 
 if config["solver"]["ParametricStudy"]: 
     if not config["solver"]["FrozenMesh"]:
-        ROM_model.UnFreeze_Mesh()                                               # Set space mesh cordinates as trainable
+        ROM_model.UnFreeze_Mesh()                                                       # Set space mesh cordinates as trainable
     if not config["solver"]["FrozenParaMesh"]:
-        ROM_model.UnFreeze_MeshPara()                                           # Set parameters mesh cordinates as trainable
+        ROM_model.UnFreeze_MeshPara()                                                   # Set parameters mesh cordinates as trainable
 
     ROM_model.TrainingParameters(   loss_decrease_c = config["training"]["loss_decrease_c"], 
                                     Max_epochs = config["training"]["n_epochs"], 
@@ -261,13 +256,8 @@ if config["solver"]["ParametricStudy"]:
         optimizer = torch.optim.Adam([p for p in ROM_model.parameters() if p.requires_grad], lr=config["training"]["learning_rate"])
         match config["interpolation"]["dimension"]:
             case 1:
-                # Training_NeuROM(ROM_model,config,optimizer)                     # First stage of training (ADAM)
-                # # WARNING: No collision check with free mesh yet
-                # Training_NeuROM_FinalStageLBFGS(ROM_model,config)               # Second stage of training (LBFGS)
                 ROM_model, Mesh_object = Training_NeuROM_multi_level(ROM_model,config, Mat)         
             case 2:
-                # Training_NeuROM(ROM_model, config, optimizer, Mat)              # First stage of training (ADAM)
-                # Training_NeuROM_FinalStageLBFGS(ROM_model,config, Mat)               # Second stage of training (LBFGS)
                 ROM_model, Mesh_object = Training_NeuROM_multi_level(ROM_model,config, Mat)         
         ROM_model.to(torch.device("cpu"))
         ROM_model.eval()
@@ -323,19 +313,19 @@ if config["solver"]["ParametricStudy"]:
                     requires_grad=True)
 
     if config["training"]["TrainingRequired"]:
-        if min(ROM_model.training_recap["Loss_vect"]) > 0:                      # Find sign of the converged loss
+        if min(ROM_model.training_recap["Loss_vect"]) > 0:                              # Find sign of the converged loss
             sign = "Positive"
         else:
             sign = "Negative"
-    if config["solver"]["N_ExtraCoordinates"] == 2:                             # define type of parametric study for saving files
+    if config["solver"]["N_ExtraCoordinates"] == 2:                                     # define type of parametric study for saving files
         Study = "_BiPara"
     else: 
         Study = "_MonoPara" 
     
     if config["training"]["LoadPreviousModel"]:
-        Initialisation_state = "_Initialised_"                                  # Initialised from previous model
+        Initialisation_state = "_Initialised_"                                          # Initialised from previous model
     else:
-         Initialisation_state = "_Raw_"                                         # Trained from scratch
+         Initialisation_state = "_Raw_"                                                 # Trained from scratch
     if config["solver"]["FrozenMesh"]:
         Mesh_state = '_FrozenMesh_'
     else:
@@ -343,7 +333,7 @@ if config["solver"]["ParametricStudy"]:
 
     val = str(np.format_float_scientific(config["training"]["loss_decrease_c"], precision=2))            # Convergence criterion
 
-    if config["postprocess"]["Plot_loss_mode"]:                                 # Plot loss and modes
+    if config["postprocess"]["Plot_loss_mode"]:                                         # Plot loss and modes
         Pplot.Plot_PosNegLoss_Modes(ROM_model.training_recap["Mode_vect"],ROM_model.training_recap["Loss_vect"],
                                     'Loss_Modes'+"_"+
                                     config["geometry"]["Name"]+"_"+
@@ -355,7 +345,7 @@ if config["solver"]["ParametricStudy"]:
                                     Initialisation_state+
                                     val
                                     , sign = sign,tikz = True, Zoom_required = True)
-    if config["postprocess"]["Plot_loss_decay_mode"]:                           # Plot loss rate and modes
+    if config["postprocess"]["Plot_loss_decay_mode"]:                                   # Plot loss rate and modes
         Pplot.Plot_Lossdecay_Modes(ROM_model.training_recap["Mode_vect"],
                                     ROM_model.training_recap["Loss_decrease_vect"],
                                     'Loss_rate_Modes'+"_"+
@@ -370,7 +360,7 @@ if config["solver"]["ParametricStudy"]:
                                     config["training"]["loss_decrease_c"],
                                     True)
 
-    if config["postprocess"]["Plot_error_mode"]:                           # Plot loss rate and modes
+    if config["postprocess"]["Plot_error_mode"]:                                        # Plot loss rate and modes
         Pplot.Plot_L2error_Modes(ROM_model.training_recap["Mode_vect"],
                                     ROM_model.training_recap["L2_error"],
                                     'L2_error_Modes'+"_"+

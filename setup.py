1cb3dc39e75436c60d597e9ac2778dd8df3c6c43
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2024-09-12 21:59:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AlexandreDabySeesaram/NeuROM",
     packages=setuptools.find_packages(include=['neurom', 'neurom.*']),                            # Automatically finds neurom and its subpackages
+    scripts=["main",],
     license="GPLv3",
     classifiers=[
         "Programming Language :: Python :: 3",

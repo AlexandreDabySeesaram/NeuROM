aae8ef50300917c7afcdbfb52943184756b7e913
Alexandre Daby-Seesaram <alexandre.daby-seesaram@polytechnique.edu>
2024-09-27 19:38:11 +0200

    Fix no metadata error

--- neurom/__init__.py
+++ neurom/__init__.py
@@ -1,5 +1,9 @@
 from .main import main
 from importlib.metadata import version, PackageNotFoundError
-__version__ = version("neurom")
 
-__all__ = [main,__version__]
+try:
+    __version__ = version("neurom")
+
+    __all__ = [main,__version__]
+except:
+     __all__ = [main]

c6f1b816234e4dca64049aadcd0c3c4b2defbe85
Jules Dichamp <jules.dichamp@polytechnique.edu>
2026-03-03 15:01:46 +0100

    Added test for TrainableField
    Use a NoConstraint() case for BC for simplicity.
    
    Ref #13.

--- tests/unit/interpolation/test_field_interpolator.py
+++ tests/unit/interpolation/test_field_interpolator.py
@@ -30,6 +30,28 @@ def field():
     return field
 
 
+@pytest.fixture
+def trainable_field():
+    """
+    Prepare what is needed to define a TrainableField:
+    * name = "test"
+    * Simple topology: 3 elements with 4 nodes.
+    * Values: [3., 7., 6., -5.]
+    * Constraint: NoConstraint()
+    """
+    name = "test"
+    N = 4
+    nodes = torch.arange(0, N)
+    elements = torch.vstack([torch.arange(0, N - 1), torch.arange(1, N)]).T
+    topology = Topology(nodes, elements)
+    values = torch.tensor([3.0, 7.0, 6.0, -5.0])
+    field = TrainableField(
+        name="test", topology=topology, init_values=values, constraint=NoConstraint()
+    )
+
+    return field
+
+
 class TestFieldInterpolator:
     """Test FieldInterpolator class
 
@@ -72,6 +94,47 @@ class TestFieldInterpolator:
             xi = val["xi"]
             expected = val["expected"]
             interp = field_interp.at_reference(xi)
+            assert interp == pytest.approx(expected, rel=self.relative_tolerance), (
+                "xi = ",
+                xi,
+            )
+
+    def test_field_interpolator_with_trainable_field(self, trainable_field):
+        """Test the FieldInterpolator class with a TrainableField
+
+        Note: no constraint is set for the boundary conditions for simplicity.
+        """
+        # Create FieldInterpolator
+        field_interp = FieldInterpolator(sf=LinearSegment(), field=trainable_field)
+
+        # Prepare reference positions, tensor of shape (N_e, N_q, dim) and expected values
+        # Midpoints of values
+        values = [  # Midpoint
+            {
+                "xi": torch.tensor([0.0, 0.0, 0.0])[:, None, None],
+                "expected": torch.tensor([5.0, 6.5, 0.5]).unsqueeze(-1),
+            },
+            # Start
+            {
+                "xi": torch.tensor([-1.0, -1.0, -1.0])[:, None, None],
+                "expected": torch.tensor([3.0, 7.0, 6.0]).unsqueeze(-1),
+            },
+            # End
+            {
+                "xi": torch.tensor([1.0, 1.0, 1.0])[:, None, None],
+                "expected": torch.tensor([7.0, 6.0, -5.0]).unsqueeze(-1),
+            },
+            # Mix
+            {
+                "xi": torch.tensor([0.5, -0.75, -0.5])[:, None, None],
+                "expected": torch.tensor([6.0, 6.875, 3.25]).unsqueeze(-1),
+            },
+        ]
+
+        for val in values:
+            xi = val["xi"]
+            expected = val["expected"]
+            interp = field_interp.at_reference(xi).detach()
             assert interp == pytest.approx(expected, rel=self.relative_tolerance), (
                 "xi = ",
                 xi,

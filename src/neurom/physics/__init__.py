de47c5f76872963975b9297b8661cacdc3d8c180
Jules Dichamp <jules.dichamp@polytechnique.edu>
2026-03-30 14:47:13 +0200

    Removed barrier potential
    Now replaced by flip loss

--- src/neurom/physics/__init__.py
+++ src/neurom/physics/__init__.py
@@ -1,4 +1,3 @@
 from neurom.physics.term import Term
 from neurom.physics.elastic_energy import ElasticEnergy
 from neurom.physics.load_potential import LoadPotential
-from neurom.physics.barrier_potential import BarrierPotential
